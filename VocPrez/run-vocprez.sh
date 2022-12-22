eaa6efac181aa15be489158ccd1fc899e1f53e17
Alejandro Villar <avillar@ogc.org>
2022-12-22 16:54:58 +0100

    Add or overwrite VocPrez configuration

--- VocPrez/run-vocprez.sh
+++ VocPrez/run-vocprez.sh
@@ -7,4 +7,8 @@ chmod a+x apply.sh
 ./apply.sh
 cd /app
 rm -f vocprez/cache/DATA.p
+if [ -f vocprez/_config/additional.py ]; then
+  cat vocprez/_config/__init__.py <(echo -e '\n### Additional configuration by Docker') vocprez/_config/additional.py > /vocprez_config.py
+  mv /vocprez_config.py vocprez/_config/__init__.py
+fi
 exec gunicorn -b 0.0.0.0:8000 vocprez.app:app
