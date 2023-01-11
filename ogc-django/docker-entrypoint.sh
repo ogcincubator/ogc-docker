0b368ae1a4b4f25af2474f0fe023d9b789a418c9
Alejandro Villar <avillar@ogc.org>
2023-01-11 14:11:23 +0100

    [Django] Fix entrypoint command

--- ogc-django/docker-entrypoint.sh
+++ ogc-django/docker-entrypoint.sh
@@ -16,9 +16,12 @@ export SETTINGS_FILE="/app/settings-base.py"
 for CONFVAR in DEBUG URIREDIRECT_ALTR_BASETEMPLATE SMUGGLER_FIXTURE_DIR STATIC_ROOT ALLOWED_HOSTS ; do
   VARNAME="DJANGO_${CONFVAR}"
   eval CONFVALUE="\$${VARNAME}"
-  grep -q ^${CONFVAR} "${SETTINGS_FILE}" \
-    && sed -r "s/^${CONFVAR}\s+=.*/${CONFVAR} = ${CONFVALUE}/" -i "${SETTINGS_FILE}" \
-    || echo "${CONFVAR} = ${CONFVALUE}" >> "${SETTINGS_FILE}"
+  if [ -n "${CONFVALUE}" ]; then
+    echo "Setting configuration ${CONFVAR} = ${CONFVALUE}"
+    grep -q ^${CONFVAR} "${SETTINGS_FILE}" \
+      && sed -r "s/^${CONFVAR}\s+=.*/${CONFVAR} = ${CONFVALUE}/" -i "${SETTINGS_FILE}" \
+      || echo "${CONFVAR} = ${CONFVALUE}" >> "${SETTINGS_FILE}"
+  fi
 done
 
 python /app/install-modules.py
@@ -35,7 +38,7 @@ if [ "${WITH_NGINX}" = "1" -o "${WITH_NGINX}" = "true" ]; then
   gunicorn --bind unix:/run/gunicorn.sock --access-logfile - "${PROJECT_NAME}.wsgi:application" --daemon
   CMD="nginx -g 'daemon off;'"
 else
-  CMD="gunicorn --bind unix:/run/gunicorn.sock --access-logfile - ${PROJECT_NAME}.wsgi:application"
+  CMD="gunicorn --bind 0.0.0.0:80 --access-logfile - ${PROJECT_NAME}.wsgi:application"
 fi
 
 if [ $# -gt 0 ]; then
